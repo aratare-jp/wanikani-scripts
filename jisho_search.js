8f9dc9df23e63936115e40762e3dffb7b0305a58
Rex Truong <rextruong@pm.me>
2020-08-08 11:54:57 +0700

    Added jisho search script

---
+++ jisho_search.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Jisho Search
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *
+// @grant        GM_openInTab
+// @run-at       context-menu
+
+// ==/UserScript==
+
+(function (window) {
+    'use strict';
+
+    function selection() {
+        if (window.getSelection) {
+            return window.getSelection().toString();
+        } else {
+            return null;
+        }
+    }
+
+    var selected = selection();
+
+    GM_openInTab("https://jisho.org/search/" + encodeURIComponent(selected), {active: true});
+})(window);
