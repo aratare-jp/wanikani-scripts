2752e7ceb4222360af18193179772bdf37d62f3a
Rex Truong <rextruong@pm.me>
2021-04-29 17:53:35 +0700

    Fixed outdated URL causing Jisho Search to not working properly

--- jisho_search.js
+++ jisho_search.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *
+// @match        *://*/*
 // @grant        GM_openInTab
 // @run-at       context-menu
 
